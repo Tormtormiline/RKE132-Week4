658a87c8d1fdf4073636efd43adccfe0d88a4084
Tormtormiline <toivo.luhaaar@tktk.ee>
2024-03-07 20:41:37 +0200

    first commit

---
+++ Week4/Program.cs
@@ -0,0 +1,15 @@
+﻿
+//FOR-LOOP - 
+//DRY-principle - Do not repeat yourself 
+
+int i = 0;
+
+for (int i = 0; i < 5; i++)  // on sama asi mis i++ > i+1
+{
+         Console.WriteLine("We don't need no education");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day.");
+
+
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+
+
+int i = 0;
+
+for (int i = 0; i < 5; i++) ;
+{ 
+    Console.WriteLine("We don't need no education");
+}
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day");
